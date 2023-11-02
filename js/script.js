189b94f66f544fc728e4671ba628d6a3e01878e0
Artem <artem.ugolkov@ukr.net>
2023-11-02 14:37:03 +0200

    logic

--- js/script.js
+++ js/script.js
@@ -91,49 +91,127 @@ function closeCart() {
 const itemsButtons = document.querySelectorAll('.pods_button');
 const cartSubIcon = document.querySelector('.cart_number');
 const cartWrapper = document.querySelector('.cart');
-
+const totalPrice = document.querySelector('.cart_total_amount');
+const item = document.querySelector('.cart_item');
 itemsButtons.forEach((e) => {
   e.addEventListener('click', (event) => {
-    cartSubIcon.classList.add('cart_number_visual');
     cartWrapper.classList.add('open_cart');
     const imageLink = event.target.parentElement.previousElementSibling.children[0].src;
     const itemTitle = event.target.parentElement.children[0].children[0].innerHTML;
     const itemPrice = event.target.parentElement.children[0].children[1].children[0].innerHTML;
+    console.log(itemTitle);
 
-    const cartItemWrapper = document.querySelector('.cart_items_wrapper');
-    const cartItem = document.createElement(`div`);
-    cartItem.classList.add('cart_item');
-    cartItem.innerHTML = `
-      
-        <div class="cart_item_content_wrapper">
-          <div class="cart_item_info">
-            <img src="${imageLink}" alt="" class="cart_item_image" />
-            <div class="cart_item_text">
-              <p class="cart_item_text_name">${itemTitle}</p>
-              <p class="cart_item_text_available">В наявності</p>
-              <p class="cart_item_text_price">${itemPrice}₴/ шт</p>
-            </div>
+    if (cartWrapper.innerHTML.includes(itemTitle)) {
+      cartWrapper.classList.add('open_cart');
+    } else {
+      const cartItemWrapper = document.querySelector('.cart_items_wrapper');
+      const cartItem = document.createElement(`div`);
+      cartItem.classList.add('cart_item');
+      cartItem.innerHTML = `
+    <div class="cart_item_content_wrapper">
+      <div class="cart_item_info">
+        <img src="${imageLink}" alt="" class="cart_item_image" />
+        <div class="cart_item_text">
+          <p class="cart_item_text_name">${itemTitle}</p>
+          <p class="cart_item_text_available">В наявності</p>
+          <div class="total_amount_wrapper">
+            <p class="cart_item_text_price"><span>${itemPrice}</span><span class="grn cart_item_text_price">₴</span></p>
           </div>
-          <img src="img/cart/bin.svg" alt="" class="cart_item_bin" />
         </div>
-        <div class="cart_quantity_total_wrapper">
-          <div class="cart_quantity_wrapper">
-            <span class="cart_minus">-</span>
-            <span class="cart_quantity">1</span>
-            <span class="cart_plus">+</span>
-          </div>
-          <span class="cart_item_price">${itemPrice}₴</span>
-        </div>
+      </div>
+      <img src="img/cart/bin.svg" alt="" class="cart_item_bin" />
+    </div>
+    <div class="cart_quantity_total_wrapper">
+      <div class="cart_quantity_wrapper">
+        <span class="cart_minus">-</span>
+        <span class="cart_quantity">1</span>
+        <span class="cart_plus">+</span>
+      </div>
+      <div class="total_amount_wrapper">
+      <span class="cart_item_price">${itemPrice}</span><span class="grn cart_item_price">₴</span>
       
-    `;
-    cartItemWrapper.appendChild(cartItem);
-    const cartBins = document.querySelectorAll('.cart_item_bin');
-    cartBins.forEach((e) => {
-      e.addEventListener('click', (event) => {
-        event.target.parentElement.parentElement.remove();
-      });
-    });
-    const totalPrice = document.querySelector('.cart_total_amount');
-    totalPrice.innerHTML = Number(totalPrice.innerHTML) + Number(itemPrice);
+    </div>
+`;
+      cartItemWrapper.appendChild(cartItem);
+
+      const totalPrice = document.querySelector('.cart_total_amount');
+
+      totalPrice.innerHTML = Number(totalPrice.innerHTML) + Number(itemPrice);
+
+      const item = document.querySelector('.cart_item');
+      cartSubIcon.innerHTML = Number(cartSubIcon.innerHTML) + 1;
+      cartItemWrapper.contains(item)
+        ? cartSubIcon.classList.add('cart_number_visual')
+        : cartSubIcon.classList.remove('cart_number_visual');
+    }
   });
 });
+const wrapper = document.querySelector('.cart_items_wrapper');
+
+wrapper.addEventListener('click', function (e) {
+  let quantity = Number(e.target.parentElement.children[1].innerHTML);
+
+  if (e.target.classList.contains('cart_plus')) {
+    quantity++;
+    e.target.parentElement.children[1].innerHTML = quantity;
+    e.target.parentElement.parentElement.children[1].children[0].innerHTML =
+      Number(
+        e.target.parentElement.parentElement.parentElement.children[0].children[0].children[1]
+          .children[2].children[0].children[0].innerHTML,
+      ) * quantity;
+
+    totalPrice.innerHTML =
+      Number(totalPrice.innerHTML) +
+      Number(
+        e.target.parentElement.parentElement.parentElement.children[0].children[0].children[1]
+          .children[2].children[0].children[0].innerHTML,
+      );
+  } else if (e.target.classList.contains('cart_minus')) {
+    if (quantity > 1) {
+      quantity--;
+      e.target.parentElement.children[1].innerHTML = quantity;
+      e.target.parentElement.parentElement.children[1].children[0].innerHTML =
+        Number(
+          e.target.parentElement.parentElement.parentElement.children[0].children[0].children[1]
+            .children[2].children[0].children[0].innerHTML,
+        ) * quantity;
+      totalPrice.innerHTML =
+        Number(totalPrice.innerHTML) -
+        Number(
+          e.target.parentElement.parentElement.parentElement.children[0].children[0].children[1]
+            .children[2].children[0].children[0].innerHTML,
+        );
+    }
+  } else if (e.target.classList.contains('cart_item_bin')) {
+    e.target.parentElement.parentElement.remove();
+    cartSubIcon.innerHTML = Number(cartSubIcon.innerHTML) - 1;
+
+    totalPrice.innerHTML =
+      Number(totalPrice.innerHTML) -
+      Number(e.target.parentElement.parentElement.children[1].children[1].children[0].innerHTML);
+    wrapper.contains(item)
+      ? cartSubIcon.classList.add('cart_number_visual')
+      : cartSubIcon.classList.remove('cart_number_visual');
+  }
+});
+
+// const minuses = document.querySelectorAll('.cart_minus');
+// const pluses = document.querySelectorAll('.cart_plus');
+// const quantities = document.querySelectorAll('.cart_quantity');
+// console.log(minuses, pluses, quantities);
+
+// pluses.forEach((e) => {
+//   e.addEventListener('click', (event) => {
+//     console.log(event.target.parentElement.children[1].innerHTML);
+//   });
+// });
+
+// cartBins.forEach((e, i) => {
+//   e.addEventListener('click', (event) => {
+//     event.target.parentElement.parentElement.remove();
+//     const item = document.querySelector('.cart_item');
+//     cartItemWrapper.contains(item)
+//       ? cartSubIcon.classList.add('cart_number_visual')
+//       : cartSubIcon.classList.remove('cart_number_visual');
+//   });
+// });
